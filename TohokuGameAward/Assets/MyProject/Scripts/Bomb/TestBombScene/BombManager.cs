e3a006b5b353f12b56245e86becc7b24a15de2c7
kohnokouta <xiangyehangda@gmail.com>
2024-10-22 16:43:17 +0900

    [Add]MombSettingを作成

--- TohokuGameAward/Assets/MyProject/Scripts/Bomb/TestBombScene/BombManager.cs
+++ TohokuGameAward/Assets/MyProject/Scripts/Bomb/TestBombScene/BombManager.cs
@@ -1,115 +1,36 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BombManager : MonoBehaviour
 {
-    [Header("爆発までの時間[s]")]
-    [SerializeField]
-    private float m_time = 3.0f;
+    // シングルトンのインスタンス
+    public static BombManager Instance { get; private set; }
 
-    [Header("爆風に当たったときに吹っ飛ぶ力の強さ")]
-    [SerializeField]
-    private float m_power = 1;
+    public List<BombData> allBombData;
 
-    [Header("爆発の当たる範囲")]
-    [SerializeField]
-    private float m_size = 2;
-
-    [Header("回転の中心点")]
-    [SerializeField]
-    private float m_pivot = -1;
-
-    [Header("爆風のPrefab")][SerializeField] private Explosion m_explosionPrefab;
-
-    [Header("爆弾のコライダー")]
-    [SerializeField]
-    private SphereCollider m_collider;
-
-    [Header("爆弾のモデル")]
-    [SerializeField]
-    private GameObject m_bombModel;
-
-    [SerializeField]
-    private BombController m_bombController;
-
-    private PlayerMover m_playerMover;
-
-    public bool isThrown = false;       //爆弾が投射されたかどうかを追跡
-    private bool isRowling = false;     //回転中かどうか
-
-    private int m_playerNumber = -1;    //プレイヤー番号保存用
-
-    private void Update()
+    void Awake()
     {
-        if (isRowling)
+        // すでにインスタンスが存在している場合は破棄
+        if (Instance != null && Instance != this)
         {
-            m_bombController.Rowling();
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // シーンを跨いでも保持する場合
         }
     }
 
-    public void FuseOn()
+    public BombData GetBombDataByGenre(BombData.BombGenre genre)
     {
-        // 一定時間経過後に発火
-        Invoke(nameof(Explode), m_time);
-    }
-
-    public void ThrowBomb()
-    {
-        if (!isThrown)
+        foreach (var bombData in allBombData)
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
-            m_collider.center = new Vector3(0, m_pivot, 0);
-            m_bombModel.transform.localPosition = new Vector3(0, m_pivot, 0);
-            m_bombController.Throw();
-            isThrown = true;
-            isRowling = true;
+            if (bombData.bombGenre == genre)
+            {
+                return bombData;
+            }
         }
-    }
-
-    private void Explosion()
-    {
-        //即爆破
-        Explode();
-    }
-
-    private void Explode()
-    {
-        // 爆発を生成
-        var explosion = Instantiate(m_explosionPrefab, m_collider.transform.position, Quaternion.identity);
-        //威力の設定
-        explosion.Explode(m_power, m_size);
-
-        // 自身は消える
-        Destroy(gameObject);
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        //なにかに触れたら
-        isRowling = false;
-
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Wall"))
-        {
-            m_collider.center = new Vector3(0, 0, 0);
-            m_bombModel.transform.localPosition = new Vector3(0, 0, 0);
-            isThrown = false;
-            m_playerNumber = -1;
-        }
-
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject.tag == "Player")
-        {
-            m_playerMover = other.gameObject.GetComponent<PlayerMover>();
-            //if (m_playerNumber == -1 || m_playerNumber == m_playerMover.playerNomber)
-            //{
-            //    m_playerNumber = m_playerMover.playerNomber;
-            //}
-            //else
-            //{
-            //    Explosion();
-            //}
-        }
+        return null;
     }
 }
