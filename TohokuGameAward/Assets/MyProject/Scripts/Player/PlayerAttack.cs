7bc1b65e5132246cfb1bf79d647a336d90606846
Sasaki Yu <yusasaki34@icloud.com>
2024-10-27 12:52:38 +0900

    [Add]爆弾を持って投げる挙動の追加
    スティック入力によって投げ方が変化

--- TohokuGameAward/Assets/MyProject/Scripts/Player/PlayerAttack.cs
+++ TohokuGameAward/Assets/MyProject/Scripts/Player/PlayerAttack.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Xml.Serialization;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -15,7 +12,7 @@ public class PlayerAttack : MonoBehaviour
     [SerializeField]
     private Rigidbody m_playerRigidbody;
     [SerializeField]
-    public PlayerMover m_gamepadNomebr;
+    public PlayerMover m_playerMoverScript;
     [SerializeField]
     private float m_playerAttackPower = 1;
     [SerializeField]
@@ -27,9 +24,11 @@ public class PlayerAttack : MonoBehaviour
 
     private bool m_isAttack = false;
     private bool m_isStun = false;
+    //private bool m_isOnGround = false;
 
     Vector2 m_leftStick;
     Vector2 m_rightStik;
+    Vector2 m_attackVector;
 
     // Start is called before the first frame update
     void Start()
@@ -40,24 +39,22 @@ public class PlayerAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        m_playerArmTransform.position = m_playerTransform.position;
         var gamepad = Gamepad.current;
         if (gamepad == null)
             return;
 
-        var padCurrent = Gamepad.all.Count;
+        m_playerArmTransform.position = m_playerTransform.position;
+     　 var padCurrent = Gamepad.all.Count;
         Vector2 playerPosition = m_playerTransform.position;
         m_playerArmTransform.rotation = Quaternion.identity;
         for (int i = 0; i < padCurrent; i++)
         {
-            if(i == m_gamepadNomebr.m_playerNomber)
+            if(i == m_playerMoverScript.PlayerNumber)
             {
-                //コントローラーのLスティック
-                if (!m_isAttack)
-                {
+                 //コントローラー入力取得
                     m_leftStick = Gamepad.all[i].leftStick.ReadValue();
                     m_rightStik = Gamepad.all[i].rightStick.ReadValue();
-                }
+
                 if (Gamepad.all[i].xButton.wasPressedThisFrame && !m_isAttack && !m_isStun)
                 {
                     m_isAttack = true;
@@ -65,6 +62,7 @@ public class PlayerAttack : MonoBehaviour
                 }
                 if (m_isAttack)
                 {
+                    //パンチの仮オブジェクト判定用
                     var normalizedLeftStick = m_leftStick.normalized;
                     m_playerArmTransform.position = playerPosition + normalizedLeftStick * m_playerAttackRange;
                 }
@@ -81,7 +79,6 @@ public class PlayerAttack : MonoBehaviour
             m_playerRigidbody.AddForce(normalizedPlayerArm.x * m_playerAttackPower, normalizedPlayerArm.y * m_playerAttackPower, 0, ForceMode.Impulse);
             m_isStun = true;
             Invoke(nameof(StopPlayerAttack), m_playerStunTime);
-            //Debug.Log(normalizedPlayerArm);
         }
     }
     private void StopPlayerAttack()
--- TohokuGameAward/Assets/MyProject/Scripts/Player/PlayerMover.cs
+++ TohokuGameAward/Assets/MyProject/Scripts/Player/PlayerMover.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using System.Linq;
 
 public class PlayerMover : MonoBehaviour
 {
@@ -8,23 +7,53 @@ public class PlayerMover : MonoBehaviour
     private Transform m_playerTransform;
     [SerializeField]
     private Rigidbody m_playerRigidbody;
-    [SerializeField]
-    public int m_playerNomber;
-
+    [Header("コントローラー番号")]
+    [SerializeField] private int m_playerNumber;
+    public int PlayerNumber { get { return m_playerNumber; } }
+    [Header("プレイヤーの移動速度")]
     [SerializeField]
     private float     m_playerMoveSpeed;
+    [Header("プレイヤーのジャンプ力")]
     [SerializeField]
     private Vector3   m_playerJumpScale;
+    [Header("ボムの回転速度")]
+    [SerializeField]
+    private Vector3 m_bombRotationSpeed;
+    [Header("ボムの発射角度")]
+    [SerializeField]
+    private float m_bombForceAngle;
+    [Header("ボムの投げる力")]
+    [SerializeField]
+    private float m_bombThrowPower = 44;
+    private Vector3 m_bombThrow;
 
-    private bool m_playerGroundChecker = false;
+    [SerializeField]
+    private float m_bombCoolTime = 0.1f;
 
+    private float m_stickRange = 0.7f;
+
+    private bool m_isPlayerGroundChecker = false;
+    private bool m_isGetBomb = false;
+    private bool m_isThrowBomb = false;
+    private bool m_isPlayerLookLeft = true;
     public static int m_gamepadNomber;
+    [Header("ボムの横投げ角度")]
+    [SerializeField]
+    private float m_bombThrowSide = 30.0f;
+    [Header("ボムの上投げ角度")]
+    [SerializeField]
+    private float m_bombThrowUp = 80.0f;
+    [Header("ボムの下投げ角度")]
+    [SerializeField]
+    private float m_bombThrowDown = -70.0f;
 
-
+    private GameObject m_bombObject;
+    private Rigidbody m_bombRigidbody;
+    private Collider m_bombCollider;
     // Start is called before the first frame update
     void Start()
     {
-        m_gamepadNomber = m_playerNomber;
+        m_gamepadNomber = PlayerNumber;
     }
 
     // Update is called once per frame
@@ -33,35 +62,113 @@ public class PlayerMover : MonoBehaviour
         var gamepad = Gamepad.current;
         if (gamepad == null)
             return;
-
-        var padCurrent = Gamepad.all.Count;
-        Vector2 playerPositionVector2 = transform.position;
         m_playerTransform.rotation = Quaternion.identity;
+        var padCurrent = Gamepad.all.Count;
+        //プレイヤー位置取得
+        Vector2 playerPositionVector2 = transform.position;
 
         for (int i = 0; i < padCurrent; i++)
         {
-            if(i == m_playerNomber)
+            if(i == PlayerNumber)
             {
-                //地面に接触したらジャンプ
-                if (Gamepad.all[i].aButton.wasPressedThisFrame && m_playerGroundChecker == true)
-                {
-                    //Debug.Log(m_playerNomber);
-                    m_playerRigidbody.AddForce(m_playerJumpScale, ForceMode.Impulse);
-                }
                 //左右移動
                 var leftStick = Gamepad.all[i].leftStick.ReadValue();
                 playerPositionVector2.x += m_playerMoveSpeed * leftStick.x * Time.deltaTime;
                 m_playerTransform.position = playerPositionVector2;
+                //地面に接触したらジャンプ
+                if (Gamepad.all[i].aButton.wasPressedThisFrame && m_isPlayerGroundChecker == true)
+                {
+                    //Debug.Log(m_playerNomber);
+                    m_playerRigidbody.AddForce(m_playerJumpScale, ForceMode.Impulse);
+                }
+                //ボムをプレイヤーに持たせる
+                if (m_isThrowBomb)
+                {
+                    Vector3 bombPosition = m_playerTransform.position;
+                    bombPosition.y = m_playerTransform.position.y + m_playerTransform.localScale.y;
+                    m_bombObject.transform.position = bombPosition;
+                    if ((Gamepad.all[i].rightTrigger.wasPressedThisFrame))
+                    {
+                        ThrowBomb(leftStick);
+                    }
+                }
+                //プレイヤー向きを判定
+                if (leftStick.x >= 0.0f)
+                {
+                    m_isPlayerLookLeft = true;
+                }
+                else if (leftStick.x <= 0.0f)
+                {
+                    m_isPlayerLookLeft = false;
+                }
+                //ボムの角度決定
+                if (leftStick.x >= m_stickRange)
+                {
+                    m_bombForceAngle = m_bombThrowSide;
+                }
+                else if (leftStick.x <= -m_stickRange)
+                {
+                    m_bombForceAngle = 180.0f - m_bombThrowSide;
+                }
+                else if (leftStick.y <= -m_stickRange && !m_isPlayerLookLeft)
+                {
+                    m_bombForceAngle = 180.0f - m_bombThrowDown;
+                }
+                else if (leftStick.y >= m_stickRange && !m_isPlayerLookLeft)
+                {
+                    m_bombForceAngle = 180.0f - m_bombThrowUp;
+                }
+                else if (leftStick.y <= -m_stickRange && m_isPlayerLookLeft)
+                {
+                    m_bombForceAngle = m_bombThrowDown;
+                }
+                else if (leftStick.y >= m_stickRange && m_isPlayerLookLeft)
+                {
+                    m_bombForceAngle = m_bombThrowUp;
+                }
             }
         }
     }
+
+    private void ThrowBomb(Vector2 stickVector)
+    {
+        Invoke(nameof(BombCoolTimeEnd), m_bombCoolTime);
+        m_bombRigidbody.useGravity = true;
+        m_isThrowBomb = false;
+
+        //角度をラジアン値に変換
+        
+        float bombRad = m_bombForceAngle * Mathf.PI / 180f;
+        m_bombThrow.x = Mathf.Cos(bombRad);
+        m_bombThrow.y = Mathf.Sin(bombRad);
+        m_bombRigidbody.AddTorque(m_bombRotationSpeed, ForceMode.VelocityChange);
+        m_bombRigidbody.AddForce(m_bombThrow * m_bombThrowPower, ForceMode.Impulse);
+    }
+    private void BombCoolTimeEnd()
+    {
+        m_bombCollider.enabled = true;
+        m_isGetBomb = false;
+    }
     private void OnCollisionStay(Collision collision)
     {
+        //ステージ上にいるとき
         if(collision.gameObject.tag == "Stage")
-            m_playerGroundChecker = true;
+            m_isPlayerGroundChecker = true;
+        //ボムオブジェ取得
+        if(collision.gameObject.tag == "Bomb" && !m_isGetBomb)
+        {
+            m_isGetBomb = true;
+            m_isThrowBomb = true;
+            collision.collider.enabled = false;
+            collision.rigidbody.useGravity = false;
+            m_bombObject = collision.gameObject;
+            m_bombRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            m_bombCollider = collision.gameObject.GetComponent<Collider>();
+        }
     }
     private void OnCollisionExit()
     {
-        m_playerGroundChecker = false;
+        //ステージ上にいないとき
+        m_isPlayerGroundChecker = false;
     }
 }
