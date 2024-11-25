d3eabe0c55b79e97c5dcd092537eab4b21b8dfb4
harunanagaoka <shinkaigyo428@i.softbank.jp>
2024-11-25 17:30:05 +0900

    [Add]追加タスク
    範囲外に出るとプレイヤーをDestroyする機能の実装、残ったキャラに応じたテキストを表示するようにしました。

--- TohokuGameAward/Assets/MyProject/Scripts/GameManager/GameManager.cs
+++ TohokuGameAward/Assets/MyProject/Scripts/GameManager/GameManager.cs
@@ -1,42 +1,94 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField]
-    GameObject m_gameSetText = null;
+    private GameObject[] m_Players = null;
 
-    int m_count = 0;
-    string m_tagName = "Player";
+    [SerializeField]
+    Text m_gameSetText = null;
 
-    void Update()
+    [SerializeField]
+    private string m_winText = null;
+
+    [SerializeField]
+    private Vector3 m_stageRange = Vector3.zero;
+
+    private int m_currentPlayerCount = 0;
+
+    
+
+    //ステージ範囲設定用
+    private Bounds m_bounds = new Bounds(Vector3.zero,Vector3.zero);
+    private Vector3 m_center = Vector3.zero;
+
+    private void Start()
     {
-        m_count = CountPlayers();
+       m_bounds = new Bounds(m_center, m_stageRange);
+    }
+    private void Update()
+    {
+        m_currentPlayerCount = CountPlayers();
+        CheckPlayerOut();
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            DestroyPlayers();
-        }
-
-        if (m_count == 0)
+        if (m_currentPlayerCount <= 1)
         {
             GameSet();
         }
+
+        //デバッグ用 スペースボタンでデストロイ
+        DestroyPlayers();
     }
+    private void CheckPlayerOut()
+        {
+            for (int i = 0; i < m_Players.Length; i++)
+            {
+                if(m_Players[i] != null && !m_bounds.Contains(m_Players[i].transform.position))
+                {
+                    Destroy(m_Players[i]);
+                    m_Players[i] = null;
+
+                }
+            }
+        }
     private int CountPlayers()
     {
         int count = 0;
-        count = GameObject.FindGameObjectsWithTag(m_tagName).Length;
+        for(int i = 0;i < m_Players.Length; i++)
+        {
+            if (m_Players[i] != null)
+            {
+                count++;
+            }
+        }
         return count;
     }
-
-    private void DestroyPlayers()
-    {
-        Destroy(GameObject.FindWithTag(m_tagName));
-    }
-
     private void GameSet()
     {
-        m_gameSetText.SetActive(true);
+        if (m_gameSetText.gameObject.activeSelf)
+        { return; }
+
+        foreach (GameObject player in m_Players)
+        {
+            if (player != null)
+            {
+                string winnerName = player.name;
+                m_gameSetText.text = winnerName + m_winText;
+                m_gameSetText.gameObject.SetActive(true);
+                return;
+            }
+        }
+    }
+  private void DestroyPlayers()
+    {
+        string tagName = "Player";
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Destroy(GameObject.FindWithTag(tagName));
+        }
     }
 }
 
+/*
+③完了したGameManagerをPrefab化し、PrefabsフォルダにGameCircleフォルダを作成しそこ*/
