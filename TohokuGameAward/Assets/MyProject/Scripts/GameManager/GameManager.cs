d85cb6bf3a41989a24f9cd3e3bc9ba7a14465a06
harunanagaoka <shinkaigyo428@i.softbank.jp>
2024-11-25 12:28:00 +0900

    [Add]スクリプト、シーンの追加
    プレイヤーをDestroyし、0人になったらテキストが表示されるスクリプトの追加、TestGameCycleSceneの追加

---
+++ TohokuGameAward/Assets/MyProject/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject m_gameSetText = null;
+
+    int m_count = 0;
+    string m_tagName = "Player";
+
+    void Update()
+    {
+        m_count = CountPlayers();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DestroyPlayers();
+        }
+
+        if (m_count == 0)
+        {
+            GameSet();
+        }
+    }
+    private int CountPlayers()
+    {
+        int count = 0;
+        count = GameObject.FindGameObjectsWithTag(m_tagName).Length;
+        return count;
+    }
+
+    private void DestroyPlayers()
+    {
+        Destroy(GameObject.FindWithTag(m_tagName));
+    }
+
+    private void GameSet()
+    {
+        m_gameSetText.SetActive(true);
+    }
+}
+
