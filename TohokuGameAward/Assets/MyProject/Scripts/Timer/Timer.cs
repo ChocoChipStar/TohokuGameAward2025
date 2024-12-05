decb1f8ec1b83e4b90ffe1f4107aff4ffe036f92
harunanagaoka <shinkaigyo428@i.softbank.jp>
2024-12-05 13:23:18 +0900

    [Add]タイマーとCrownの追加

---
+++ TohokuGameAward/Assets/MyProject/Scripts/Timer/Timer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI m_timerText = null;
+
+    [SerializeField]
+    float m_limitTime = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RenderTimer();
+    }
+
+    void RenderTimer()
+    {
+        m_limitTime -= Time.deltaTime;
+        if (m_limitTime < 0)
+        {
+            m_limitTime = 0;
+        }
+        m_timerText.text = m_limitTime.ToString("F0");//整数で表示
+    }
+
+}
