021965df472e06b8fcdaf1b0d99856f776d4f4c5
齊藤元紀 <saitogenki20031104@gmail.com>
2024-12-10 18:47:50 +0900

    [Fix] フォルダのディレクトリ構成を修正　分別完了

--- TohokuGameAward/Assets/MyProject/Scripts/Effect/EffectManager.cs
+++ TohokuGameAward/Assets/MyProject/Scripts/Effect/EffectManager.cs
@@ -3,16 +3,24 @@
 public class EffectManager : MonoBehaviour
 {
     [SerializeField]
-    private GameObject[] m_effectObject;
+    private GameObject[] m_effectPrefab;
 
     public enum EffectType
     {
         test
     }
 
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            OnPlayEffect(Vector3.zero,EffectType.test);
+        }
+    }
+
     public void OnPlayEffect(Vector3 position, EffectType effectType)
     {
-        GameObject effect = Instantiate(m_effectObject[(int)effectType], position, Quaternion.identity);
+        GameObject effect = Instantiate(m_effectPrefab[(int)effectType], position, Quaternion.identity);
 
         EffectPlayer effectPlayer = effect.GetComponent<EffectPlayer>();
 
