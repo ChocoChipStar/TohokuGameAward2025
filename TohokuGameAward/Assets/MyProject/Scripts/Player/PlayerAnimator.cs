52421d25764125dee53d95a92c5d324dbda9e8d0
齊藤元紀 <saitogenki20031104@gmail.com>
2024-12-10 13:26:18 +0900

    [Add] アニメーション追加の為のPlayerAnimatorスクリプト追加

---
+++ TohokuGameAward/Assets/MyProject/Scripts/Player/PlayerAnimator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    [SerializeField]
+    private Animator m_animator = null;
+
+    private static readonly string[] UpperStateNames = new string[]
+    {
+        "Idle", "Move", "Jump", "PickupIdle", "PickupMove", "PickupJump"
+    };
+
+    public enum UpperState
+    {
+        Idle,
+        Move,
+        Jump,
+        PickupIdle,
+        PickupMove,
+        PickupJump,
+    }
+
+    public enum LowerState
+    {
+        Idle,
+        Move,
+        Jump
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            SetUpperBodyState(UpperState.Idle);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SetUpperBodyState(UpperState.Move);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SetUpperBodyState(UpperState.Jump);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SetUpperBodyState(UpperState.PickupIdle);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            SetUpperBodyState(UpperState.PickupMove);
+        }
+    }
+
+    public void SetUpperBodyState(UpperState state)
+    {
+        int stateNum = (int)state;
+        m_animator.SetInteger("UpperBodyState", stateNum);
+    }
+}
