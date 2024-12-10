4228e1c03ed506c3e774485239d790342ddb5ef8
kohnokouta <xiangyehangda@gmail.com>
2024-12-10 16:52:31 +0900

    [Add]EffectManager追加

---
+++ TohokuGameAward/Assets/MyProject/Scripts/Effect/EffectManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class EffectManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] m_effectObject;
+
+    public enum EffectType
+    {
+        test
+    }
+
+    public void OnPlayEffect(Vector3 position, EffectType effectType)
+    {
+        GameObject effect = Instantiate(m_effectObject[(int)effectType], position, Quaternion.identity);
+
+        EffectPlayer effectPlayer = effect.GetComponent<EffectPlayer>();
+
+        effectPlayer.OnPlay();
+    }
+}
