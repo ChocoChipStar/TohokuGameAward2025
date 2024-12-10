ca68c958509515deea2451fefc97a35916b882d4
harunanagaoka <shinkaigyo428@i.softbank.jp>
2024-12-10 12:36:11 +0900

    [Add]スクリプト　Dorone_Moover,　Dorone_Generatorの追加
    ドローン、クラウンの生成とドローンの動きを制御するスクリプトです

---
+++ TohokuGameAward/Assets/MyProject/Scripts/Dorone/Dorone_Generator.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+public class Dorone_Generator : MonoBehaviour
+{
+    [SerializeField]
+    GameObject m_doronePrefab = null;
+
+    [SerializeField]
+    GameObject[] m_crownPrefabs = null;
+
+    [SerializeField]
+    private int[] m_chanceOfCrowns = null;
+
+    [SerializeField]
+    float m_doroneSpawnTime = 0.0f;
+
+    [SerializeField]
+    Vector2 m_doroneSpawnPosition = Vector2.zero;
+
+    [SerializeField]
+    Vector2 m_crownSpawnPosition = Vector2.zero;
+
+    float m_spawnTimer = 0.0f;
+
+    int m_totalCrownChance = 0;
+
+    GameObject m_parentObject = null;
+
+    private Tuple<GameObject, int>[] m_crownData = null;
+
+    private void Start()
+    {
+        m_totalCrownChance = TotalCrownChance();
+        m_crownData = MakebombData();
+    }
+    void Update()
+    {
+        if(IsSpawnTimeReached())
+        {
+            m_parentObject = CreateDorone();
+
+            CreateCrown(m_parentObject);
+        }
+    }
+
+    private GameObject CreateDorone()
+    {
+        GameObject dorone = null;
+        dorone = Instantiate(m_doronePrefab, m_doroneSpawnPosition, Quaternion.identity,this.transform);
+        return dorone;
+    }
+
+    private void CreateCrown(GameObject dorone)
+    {
+        Instantiate(ChooseCrownGanre(MakeRandomNumber()), m_crownSpawnPosition, Quaternion.identity, dorone.transform);
+    }
+
+    private bool IsSpawnTimeReached()
+    {
+        m_spawnTimer += Time.deltaTime;
+
+        if(m_doroneSpawnTime < m_spawnTimer)
+        {
+            m_spawnTimer = 0.0f;
+
+            return true;
+        }
+        return false;
+    }
+
+    private int TotalCrownChance()
+    {
+        int totalChance = 0;
+        for (int i = 0; i < m_chanceOfCrowns.Length; i++)
+        {
+            totalChance += m_chanceOfCrowns[i];
+        }
+        return totalChance;
+    }
+
+    private Tuple<GameObject, int>[] MakebombData()
+    {
+        var makeTuple = m_crownPrefabs.Zip(m_chanceOfCrowns, (prefab, chance) => Tuple.Create(prefab, chance));
+        makeTuple.OrderByDescending(item => item.Item2);
+        var maketupleArray = makeTuple.ToArray();
+        return maketupleArray;
+    }
+
+    private int MakeRandomNumber()
+    {
+        int randomNumber = UnityEngine.Random.Range(0, m_totalCrownChance);
+        return randomNumber;
+    }
+
+    private GameObject ChooseCrownGanre(int numberToChoose)
+    {
+        int numberToChoose_ = numberToChoose;
+
+        for (int i = 0; i < m_crownPrefabs.Length; i++)
+        {
+            if (numberToChoose_ < m_crownData[i].Item2)
+            {
+                return m_crownData[i].Item1;
+            }
+            else
+            {
+                numberToChoose_ -= m_crownData[i].Item2;
+            }
+        }
+        return m_crownData[^1].Item1;
+    }
+    private void OnValidate()
+    {
+        if (m_chanceOfCrowns == null || m_crownPrefabs == null)
+        { return; }
+        if (m_chanceOfCrowns.Length != m_crownPrefabs.Length)
+        {
+            System.Array.Resize(ref m_chanceOfCrowns, m_crownPrefabs.Length);
+        }
+    }
+}
---
+++ TohokuGameAward/Assets/MyProject/Scripts/Dorone/Dorone_Moover.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class Dorone_Moover : MonoBehaviour
+{
+    [SerializeField]
+    private float m_leftEndOfTheStage_x = 0.0f;
+
+    [SerializeField]
+    private float m_rightEndOfTheStage_x = 0.0f;
+
+    [SerializeField]
+    private float m_speed = 0.0f;
+
+    private Vector3 m_currentPos = Vector3.zero;
+
+    // Update is called once per frame
+    void Update()
+    {
+        DoroneMove();
+    }
+
+    private void DoroneMove()
+    {
+        m_currentPos = this.transform.position;
+
+        if(IsAtEndOfTheStage())
+        {
+            m_speed *= -1;
+        }
+
+        m_currentPos.x += m_speed;
+
+        this.transform.position = m_currentPos;
+    }
+
+    private bool IsAtEndOfTheStage()
+    {
+        if(m_currentPos.x < m_leftEndOfTheStage_x ||
+           m_rightEndOfTheStage_x < m_currentPos.x)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
