0f4a30e6cf6e7ef4e32e828849c13fa8473122ee
齊藤元紀 <saitogenki20031104@gmail.com>
2024-12-11 16:42:55 +0900

    [Fix] シーン更新のプッシュし忘れ部分の修正パッチ 命名も少し変更

--- TohokuGameAward/Assets/MyProject/Scripts/Text/TextManager.cs
+++ TohokuGameAward/Assets/MyProject/Scripts/Text/TextManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TextAnimationManager : MonoBehaviour
+public class TextManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
