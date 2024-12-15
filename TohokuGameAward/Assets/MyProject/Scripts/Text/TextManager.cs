accd699d92cd6ba1edcb71ed38c3ab7b6c5052aa
髙橋佳吾 <keig000@docomo.ne.jp>
2024-12-15 21:28:11 +0900

    [Add]Start,Finishの連番アニメーションをTextManagerで再生出来るプログラムの実装

--- TohokuGameAward/Assets/MyProject/Scripts/Text/TextManager.cs
+++ TohokuGameAward/Assets/MyProject/Scripts/Text/TextManager.cs
@@ -1,18 +1,70 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TextManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Animator m_textAnimator = null;
+
+    public static readonly string[] AnimationName = new string[] { "Start", "Finish" };
+
+    public enum AnimationType
     {
-        
+        Start,
+        Finish
     }
 
-    // Update is called once per frame
-    void Update()
+    private bool m_isPlay = false;
+
+    private AnimationType currentType = new AnimationType();
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            OnPlay(AnimationType.Start);
+        }
+
+        if (!m_isPlay)
+        {
+            return;
+        }
+
+        if (IsFinishedAnimation())
+        {
+            OnFinish();
+        }
+    }
+
+    public void OnPlay(AnimationType type)
+    {
+        currentType = type;
+        m_textAnimator.SetBool(AnimationName[(int)currentType], true);
+        m_isPlay = true;
+    }
+
+    private void OnFinish()
+    {
+        foreach (var animationName in AnimationName)
+        {
+            m_textAnimator.SetBool(animationName, false);
+        }
+        m_isPlay = false;
+    }
+
+    private bool IsFinishedAnimation()
+    {
+        AnimatorStateInfo stateInfo = m_textAnimator.GetCurrentAnimatorStateInfo(0);
+        if (!stateInfo.IsName(AnimationName[(int)currentType]))
+        {
+            return false;
+        }
+
+        if (stateInfo.normalizedTime >= 1.0f && !stateInfo.loop)
+        {
+            return true;
+        }
+        return false;
     }
 }
+
