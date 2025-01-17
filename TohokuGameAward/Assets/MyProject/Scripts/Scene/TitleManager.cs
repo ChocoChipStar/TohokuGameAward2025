46ea81601d0a862d23fda54f0f84938d0c25d2e2
Sasaki Yu <yusasaki34@icloud.com>
2025-01-17 13:57:53 +0900

    [Add]Title,Tutorial,Confirm,のBGM,SEを追加。

--- TohokuGameAward/Assets/MyProject/Scripts/Scene/TitleManager.cs
+++ TohokuGameAward/Assets/MyProject/Scripts/Scene/TitleManager.cs
@@ -6,6 +6,11 @@ public class TitleManager : MonoBehaviour
     [SerializeField]
     private SceneChanger m_sceneChanger = null;
 
+    [SerializeField]
+    private SoundEffectManager m_soundEffectManager = null;
+
+    private bool m_isGetStart = false;
+
     private void Update()
     {
         if(Gamepad.current == null)
@@ -16,8 +21,10 @@ public class TitleManager : MonoBehaviour
         for(int i = 0; i < Gamepad.all.Count; i++)
         {
             var wasPressedStartButton = Gamepad.all[i].bButton.wasPressedThisFrame;
-            if (wasPressedStartButton)
+            if (wasPressedStartButton && !m_isGetStart)
             {
+                m_isGetStart = true;
+                m_soundEffectManager.OnPlayOneShot(SoundEffectManager.SoundEffectName.StageOut);
                 m_sceneChanger.TransitionNextScene();
             }
         }
