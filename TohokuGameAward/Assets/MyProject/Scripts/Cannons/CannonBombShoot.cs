b61a86ac7e40d7cb637140a69078c708a9a885bc
Sasaki Yu <yusasaki34@icloud.com>
2025-01-31 15:48:28 +0900

    [Add]Cannonの爆弾の軌道を描画完成。

--- TohokuGameAward/Assets/MyProject/Scripts/Cannons/CannonBombShoot.cs
+++ TohokuGameAward/Assets/MyProject/Scripts/Cannons/CannonBombShoot.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CannonBombShoot : MonoBehaviour
 {
@@ -14,26 +15,78 @@ public class CannonBombShoot : MonoBehaviour
     [SerializeField]
     private GameObject m_bombrefab = null;
 
-    private float m_shootPower = 10.0f;
+    [SerializeField,Header("爆弾発射最低距離")]
+    private float m_shootPowerMin = 2.0f;
+
+    [SerializeField, Header("爆弾発射最高距離")]
+    private float m_shootPowerMax = 2.0f;
+
+    [SerializeField, Header("爆弾の飛距離増加速度")]
+    private float m_shootChargeSpeed = 0.0f;
+
+    [SerializeField, Header("爆弾発射角度")]
+    private float m_shootQuaternion = 0.0f;
+
+    [SerializeField,Header("発射の強さ")]
+    private float m_shootPower = 0.0f;
 
     bool m_isShoot = false;
 
-
     public Vector3 ShootVelocity { get { return m_shootVelocity; } private set { m_shootVelocity = value; } }
     public Vector3 ShootInitialPosition { get { return m_shootInitialPosition; } private set { m_shootInitialPosition = value; } }
+  
+    public bool IsShoot { get { return m_isShoot; } private set { m_isShoot = value; } }
 
+    private void Start()
+    {
+        m_shootTransform.Rotate(0.0f,0.0f, m_shootQuaternion);
+        m_shootTransform.position = ShootVelocity.normalized;
+    }
 
     private void Update()
     {
-        m_shootInitialPosition = m_shootTransform.transform.position;
-        m_shootVelocity = m_shootTransform.transform.up * m_shootPower;
-
-        if (Input.GetKey(KeyCode.Space) && !m_isShoot)
+        //今はここで入力判定を行っています。
+        if (Gamepad.current.bButton.isPressed)
         {
-            //m_isShoot = true;
-            //var bomb = Instantiate(m_bombrefab, m_shootTransform.position, Quaternion.identity);
-            //var rigidbody = bomb.GetComponent<Rigidbody>();
-            //rigidbody.AddForce(ShootVelocity * rigidbody.mass, ForceMode.Impulse);
+            ShootPowerCharge();
         }
+
+        if (Gamepad.current.bButton.wasReleasedThisFrame)
+        {
+            ShootBombOperation();
+        }
+    }
+
+    /// <summary>
+    /// ボタンが押されている間、飛距離を伸ばす処理を行います。
+    /// </summary>
+    private void ShootPowerCharge()
+    {
+        if (m_shootPowerMax > m_shootPower)
+        {
+            m_shootPower += m_shootChargeSpeed * Time.deltaTime;
+        }
+        ShootInitialPosition = m_shootTransform.transform.position;
+        ShootVelocity = m_shootTransform.up * m_shootPower;
+        IsShoot = true;
+    }
+    /// <summary>
+    ///ボムを投げる処理を行います。
+    /// </summary>
+    private void ShootBombOperation()
+    {
+        var rigidbody = GenerateBomb().GetComponent<Rigidbody>();
+        rigidbody.AddForce(ShootVelocity * rigidbody.mass, ForceMode.Impulse);
+        m_shootPower = m_shootPowerMin;
+        IsShoot = false;
+    }
+
+    /// <summary>
+    /// ボムを生成し、bombのGameObjectを返します。。
+    /// </summary>
+    private GameObject GenerateBomb()
+    {
+        var bomb = Instantiate(m_bombrefab, m_shootTransform.position, Quaternion.identity);
+        return bomb;
     }
 }
