5c3078b9a73c8abce84a5f9d62483daa1e4cf8a2
齊藤元紀 <saitogenki20031104@gmail.com>
2025-02-04 18:19:06 +0900

    [Fix] 音周りのフォルダ整理とスクリプトの修正
    Audiosフォルダに名前を変更
    SoundEffectManagerとBackGroundMusicManagerをシングルトンに変更

---
+++ TohokuGameAward/Assets/MyProject/Scripts/Audios/BackGroundMusicManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundMusicManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource m_audioSource = null;
+
+    [SerializeField]
+    private List<AudioClip> m_audioClips = new List<AudioClip>();
+
+    public static BackGroundMusicManager Instance = null;
+
+    public enum MusicName
+    {
+        MainBGM,
+    }
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void OnPlayOneShot(MusicName musicNum)
+    {
+        m_audioSource.clip = m_audioClips[(int)musicNum];
+        m_audioSource.PlayOneShot(m_audioClips[(int)musicNum]);
+    }
+}
---
+++ TohokuGameAward/Assets/MyProject/Scripts/Audios/SoundEffectManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffectManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource m_audioSource = null;
+
+    [SerializeField]
+    private List<AudioClip> m_audioClips = new List<AudioClip>();
+
+    public static SoundEffectManager Instance = null;
+
+    public enum SoundEffectName
+    {
+        Death,
+        Explosion,
+        Cannon,
+        Point,
+        Ready,
+    }
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void OnPlayOneShot(SoundEffectName seNum)
+    {
+        m_audioSource.PlayOneShot(m_audioClips[(int)seNum]);
+    }
+
+    public void OnPlay(SoundEffectName seNum)
+    {
+        m_audioSource.clip = m_audioClips[(int)seNum];
+        m_audioSource.loop = true;
+        m_audioSource.Play();
+    }
+    
+    public void OnStop(SoundEffectName seNum)
+    {
+        m_audioSource.Stop();
+    }
+}
