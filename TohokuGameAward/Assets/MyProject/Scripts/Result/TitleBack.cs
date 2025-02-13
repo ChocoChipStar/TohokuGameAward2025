0dae4c7eff4b1229e613ba7d9a8087b7a95e7ba9
harunanagaoka <shinkaigyo428@i.softbank.jp>
2025-02-13 16:06:16 +0900

    [Add]リザルトアニメーション実装

---
+++ TohokuGameAward/Assets/MyProject/Scripts/Result/TitleBack.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleBack : MonoBehaviour
+{
+    [SerializeField]
+    private SceneChanger m_sceneChanger = null;
+
+    [SerializeField]
+    private float m_changeDelay = 0f;
+
+    private void ToNextScene()
+    {
+        StartCoroutine(SetNextScene());
+    }
+    private IEnumerator SetNextScene()
+    {
+        yield return new WaitForSeconds(m_changeDelay);
+        m_sceneChanger.LoadSpecifiedScene(SceneChanger.SceneName.Title);
+    }
+}
