87a34760eaf3273cc116bfbb3cc56e964ed2785b
齊藤元紀 <saitogenki20031104@gmail.com>
2025-02-14 11:57:24 +0900

    [Update] キャラクター確認画面で6人生成される不具合の修正
    PlayerGeneratorを廃止、
    PlayerManagerの生成をHumanoid,Humanoidに設定
    座標を生成後にFixecConfirmPlayerで強制的に位置修正

---
+++ TohokuGameAward/Assets/MyProject/Scripts/Players/FixedConfirmPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class FixedConfirmPlayer : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerManager m_playerManager = null;
+
+    [SerializeField]
+    private Vector2[] fixedConfirmPos = new Vector2[PlayerManager.PlayerMax];
+
+    private void Start()
+    {
+        for(int i = 0;  i < PlayerManager.PlayerMax; i++)
+        {
+            m_playerManager.Instances[i].transform.position = fixedConfirmPos[i];
+        }
+    }
+}
